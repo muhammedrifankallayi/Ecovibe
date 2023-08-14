ea2fe96a49a126883ce6a3f26d31ae7cddb479de
muhammed rifan <rifanmuhammed173@gmail.com>
2023-08-14 19:44:02 +0530

    localStorageValidation

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { HttpClient ,HttpHeaders } from '@angular/common/http';
+import { Component ,OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -9,7 +9,24 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+
+
+export class LoginComponent implements OnInit {
+
+  ngOnInit(): void {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    });
+    this.http.get('http://localhost:4000/userLogin', { headers }).subscribe((res:any)=>{
+      if(res.Authorization===true){
+        this.route.navigate(['/home'])
+      }else{
+        console.log("Not authenticated");
+        
+        this.route.navigate(['/login'])
+      }
+    });
+  }
   constructor(private route:Router , private http:HttpClient){}
 title = 'Ecovibe'
   loginData = new FormGroup({
@@ -21,7 +38,9 @@ title = 'Ecovibe'
     const FormData = this.loginData.value
    
     
-this.http.post("http://localhost:4000/userLogin",{FormData}).subscribe((res)=>{
+this.http.post("http://localhost:4000/userLogin",{FormData}).subscribe((res:any)=>{
+ localStorage.setItem("token",res.token)
+ 
  
 this.route.navigate(['/home'])
   
