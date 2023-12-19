9651ac980311d8a6b8e8d1b73336c2009f991d77
MUHAMMED RIFAN <124771141+muhammedrifankallayi@users.noreply.github.com>
2023-12-19 16:40:08 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    
+    production:true
     adminApiUrl:"https://ecovibe.online/admin",
     userApiUrl:"https://ecovibe.online",
     superAdminUrl:"https://ecovibe.online/superAdmin"
